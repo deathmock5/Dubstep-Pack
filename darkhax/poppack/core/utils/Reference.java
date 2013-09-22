5a940300df97ffdd81794af7aca48b8e18ae15b2
Darkhax <darklime@live.ca>
2013-09-22 15:17:40 -0400

    Fixed some naming stuff

--- darkhax/poppack/core/utils/Reference.java
+++ darkhax/poppack/core/utils/Reference.java
@@ -3,7 +3,7 @@ package darkhax.poppack.core.utils;
 
 public class Reference {
 	
-	public static final String MODID = "CopPack";
+	public static final String MODID = "popPack";
 	public static final String NAME = "More Records - Pop Pack";
 	public static final String VERSION = "2.0.0";
 	public static final String COMMON = "darkhax.poppack.core.proxy.CommonProxy";
